19cf75f7bb3d4fca2984994ca25759d7c2a4c5e5
Muhammad Nasif <nasifhima@gmail.com>
2025-11-08 13:46:17 +0530

    rerunning

--- services/address.service.js
+++ services/address.service.js
@@ -14,6 +14,7 @@ export const getAddressService = async (userId) => {
   return addresses;
 };
 
+
 export const editAddressService = async (id, body) => {
   const address = await addressModel.findByIdAndUpdate(id, { $set: { ...body } }, { new: true });
   return address;
